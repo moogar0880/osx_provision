19f1b23d765f3c1a77c787c5d1a0e235d79e7f7d
moogar0880 <moogar0880@gmail.com>
2014-10-12 22:11:40 -0400

    Add all the things

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,7 @@
 set -e
 
 SRC_DIRECTORY="$HOME/.deploy"
-ANSIBLE_DIRECTORY="$SRC_DIRECTORY/ansible"
-ANSIBLE_CONFIGURATION_DIRECTORY="$HOME/.ansible.d"
+ANSIBLE_DIRECTORY="$SRC_DIRECTORY/osx_provision"
 
 # Download and install Command Line Tools
 if [[ ! -x /usr/bin/gcc ]]; then
@@ -29,22 +28,8 @@ fi
 # Make the code directory
 mkdir -p ${SRC_DIRECTORY}
 
-# Clone down ansible
-if [[ ! -d ${ANSIBLE_DIRECTORY} ]]; then
-    git clone -b devel git://github.com/danieljaouen/ansible.git ${ANSIBLE_DIRECTORY}
-fi
-
-# Use the forked Ansible
-source ${ANSIBLE_DIRECTORY}/hacking/env-setup > /dev/null
-
-# Clone down the Ansible repo
-if [[ ! -d ${ANSIBLE_CONFIGURATION_DIRECTORY} ]]; then
-    git clone git@bitbucket.org:danieljaouen/ansible-base-box.git ${ANSIBLE_CONFIGURATION_DIRECTORY}
-    (cd ${ANSIBLE_CONFIGURATION_DIRECTORY} && git submodule init && git submodule update)
-fi
-
 # Provision the box
-ansible-playbook --ask-sudo-pass -i ${ANSIBLE_CONFIGURATION_DIRECTORY}/inventories/osx ${ANSIBLE_CONFIGURATION_DIRECTORY}/site.yml --connection=local
+ansible-playbook --ask-sudo-pass -i ${ANSIBLE_DIRECTORY}/inventory ${ANSIBLE_DIRECTORY}/local.yml --connection=local
 
 # Link the casks.
 ~/.bin/link-casks
